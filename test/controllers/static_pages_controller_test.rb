2c7fe83c650f24c01e4ee093dd00547bba344c0c
Colin Walker <cjwalker@sfu.ca>
2015-03-04 02:15:13 +0000

    changed routs for signup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -25,6 +25,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 end
 
 
